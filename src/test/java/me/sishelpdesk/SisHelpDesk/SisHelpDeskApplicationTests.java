c19f1a1944b3b123262be946f9e8354a2787f82c
vengaa <luisfelipeadm.ps@gmail.com>
2024-10-13 13:58:36 -0300

    Projeto criado

---
+++ src/test/java/me/sishelpdesk/SisHelpDesk/SisHelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sishelpdesk.SisHelpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisHelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
