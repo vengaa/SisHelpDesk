a7e3cd7b16f59dc45e6e64f0e80e283f35091cb6
vengaa <luisfelipeadm.ps@gmail.com>
2024-10-13 15:30:26 -0300

    Endpoint findById para tecnico

--- src/main/java/me/sishelpdesk/SisHelpDesk/controller/TecnicoController.java
+++ src/main/java/me/sishelpdesk/SisHelpDesk/controller/TecnicoController.java
@@ -1,4 +1,27 @@
 package me.sishelpdesk.SisHelpDesk.controller;
 
+import me.sishelpdesk.SisHelpDesk.domain.Tecnico;
+import me.sishelpdesk.SisHelpDesk.service.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
 public class TecnicoController {
+
+    // localhost:8080/tecnicos/1
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+        Tecnico obj = tecnicoService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
--- src/main/java/me/sishelpdesk/SisHelpDesk/service/TecnicoService.java
+++ src/main/java/me/sishelpdesk/SisHelpDesk/service/TecnicoService.java
@@ -1,4 +1,21 @@
 package me.sishelpdesk.SisHelpDesk.service;
 
+import me.sishelpdesk.SisHelpDesk.domain.Tecnico;
+import me.sishelpdesk.SisHelpDesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+
 }
